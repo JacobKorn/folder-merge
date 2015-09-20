c40b8e4198296918cf8818914057ffb236ecf896
Anthony Mace <anthony@anthonymace.com>
2015-09-20 23:51:18 +1200

    change spec expectations for different data

--- spec/models/folder_comparison_spec.rb
+++ spec/models/folder_comparison_spec.rb
@@ -3,7 +3,7 @@ require_relative '../include_helper'
 RSpec.describe FolderComparison, type: :model do
 
   let(:first_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Angela Accounting/" }
-  let(:second_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Marilyn Accounting" }
+  let(:second_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Frank Accounting" }
   let(:compare) { FolderComparison.new(first_path, second_path) }
 
   it "creates an entries hash containing files and folders" do
@@ -29,12 +29,16 @@ RSpec.describe FolderComparison, type: :model do
 
     it "creates a hash of all the files with the same path and same SHA" do
       result = compare.unchanged_files
-      expect(result.count).to eq(84)
+      expect(result.count).to eq(83)
     end
 
     it "removes the duplicate files from the original hashes" do
-      expect(compare.files_1.count).to eq(1)
-      expect(compare.files_2.count).to eq(0)
+      expect(compare.files_1.count).to eq(2)
+      expect(compare.files_2.count).to eq(1)
+    end
+
+    it "creates a hash of modified files" do
+
     end
 
   end  
