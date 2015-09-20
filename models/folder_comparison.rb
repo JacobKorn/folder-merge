66f378f3ca83bc42dbca113b32d7ceb9b81c4504
Anthony Mace <anthony@anthonymace.com>
2015-09-20 23:29:27 +1200

    building up methods of comparison process

--- models/folder_comparison.rb
+++ models/folder_comparison.rb
@@ -1,26 +1,53 @@
 class FolderComparison
 
+  attr_reader :folder_1, :folder_2, :entries_1, :entries_2, :files_1, :files_2, :unchanged_files
+
   def initialize(folder_1, folder_2)
     @folder_1 = folder_1
     @folder_2 = folder_2
-    @folder_1_entries = FolderTree.analyse(folder_1)
-    @folder_2_entries = FolderTree.analyse(folder_2)
+    @entries_1 = FolderTree.analyse(folder_1)
+    @entries_2 = FolderTree.analyse(folder_2)
   end
 
-  def compare
-    self.folder_1_files = remove_folders(folder_1_entries)
-    self.folder_2_files = remove_folders(folder_2_entries)
+  def prepare_folders
+    self.files_1 = prepare_folder(entries_1, folder_1)
+    self.files_2 = prepare_folder(entries_2, folder_2)
+  end
+
+  def unchanged_files
+    @unchanged_files ||= files_1.select do |file_1|
+      files_2.any? do |file_2|
+        file_2[:path] == file_1[:path] &&
+        file_2[:sha1] == file_1[:sha1]
+      end
+    end
+  end
+
+  def remove_unchanged_from_originals
+    self.files_1 = files_1 - unchanged_files
+    self.files_2 = files_2 - unchanged_files
   end
 
 private
-  
-  attr_accessor :folder_1_files, :folder_2_files
-  attr_reader :folder_1_entries, :folder_2_entries
+
+  attr_writer :files_1, :files_2, :unchanged_files
+
+  def prepare_folder(entries, folder)
+    files = remove_folders(entries)
+    files = remove_root_name(files, folder)
+  end
+
+  def remove_root_name(files, folder)
+    files.each do |file|
+      file[:path].slice!(folder)
+    end
+  end
 
   def remove_folders(entries)
     entries.select do |entry|
       entry[:folder] == false
     end
   end
+
 
 end
--- spec/models/folder_comparison_spec.rb
+++ spec/models/folder_comparison_spec.rb
@@ -2,25 +2,50 @@ require_relative '../include_helper'
 
 RSpec.describe FolderComparison, type: :model do
 
-  describe "#remove_folders" do
+  let(:first_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Angela Accounting/" }
+  let(:second_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Marilyn Accounting" }
+  let(:compare) { FolderComparison.new(first_path, second_path) }
 
-    let(:first_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Angela Accounting/" }
-    let(:second_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Marilyn Accounting" }
-    let(:compare) { FolderComparison.new(first_path, second_path) }
+  it "creates an entries hash containing files and folders" do
+    result = compare.entries_1.map{ |a| a[:folder] }.uniq
+    expect(result).to eq([true, false])
+  end
 
-    it "entries hash has files and folders" do
-      compare.compare
-      result = compare.folder_1_entries.map{ |a| a[:folder] }.uniq
-      expect(result).to eq([true, false])
+  describe "#prepare_folder" do
 
+    before do
+      @files = compare.send(:prepare_folder, compare.entries_1, compare.folder_1)
     end
 
-    it "files hash only has files" do
-      compare.compare
-      result = compare.folder_1_files.map { |a| a[:folder] }.uniq
-      expect(result).to eq([false])
+    it "removes the folders" do
+      result = @files.map { |file| file[:folder] }.uniq.first
+      expect(result).to eq(false)
+    end
+
+    it "removes root portion of file paths" do
+      result = @files.map { |file| file[:path].include?(compare.folder_1) }.uniq.first
+      expect(result).to eq(false)
     end
 
   end
+
+  describe "#unchanged_files" do
+
+    before do
+      compare.prepare_folders
+    end
+
+    it "creates a hash of all the files with the same path and same SHA" do
+      result = compare.unchanged_files
+      expect(result.count).to eq(84)
+    end
+
+    it "removes the duplicate files from the original hashes" do
+      compare.remove_unchanged_from_originals
+      expect(compare.files_1.count).to eq(1)
+      expect(compare.files_2.count).to eq(0)
+    end
+
+  end  
 
 end
