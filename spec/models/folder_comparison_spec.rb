52dae76140b24e08fde1b41d73bebe819804b3c6
Anthony Mace <anthony@anthonymace.com>
2015-09-24 16:46:43 +1200

    add test fixtures

--- spec/models/folder_comparison_spec.rb
+++ spec/models/folder_comparison_spec.rb
@@ -2,13 +2,13 @@ require_relative '../include_helper'
 
 RSpec.describe FolderComparison, type: :model do
 
-  let(:first_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Angela Accounting/" }
-  let(:second_path) { "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Frank Accounting" }
+  let(:first_path) { "./spec/support/diverged_folders/files" }
+  let(:second_path) { "./spec/support/diverged_folders/files_diverged" }
   let(:compare) { FolderComparison.new(first_path, second_path) }
 
   it "creates an entries hash containing files and folders" do
     result = compare.entries_1.map{ |a| a[:folder] }.uniq
-    expect(result).to eq([true, false])
+    expect(result).to include(true, false)
   end
 
   describe "#run_comparison" do
@@ -29,18 +29,28 @@ RSpec.describe FolderComparison, type: :model do
 
     it "creates a hash of all the files with the same path and same SHA" do
       result = compare.unchanged_files
-      expect(result.count).to eq(83)
+      expect(result.count).to eq(1)
     end
 
-    it "removes the duplicate files from the original hashes" do
-      expect(compare.files_1.count).to eq(2)
-      expect(compare.files_2.count).to eq(1)
+    it "removes the duplicate files from the original files_1 hash" do
+      expect(compare.files_1.count).to eq(4)
+    end
+
+    it "removes the duplicate files from the original files_2 hash" do
+      expect(compare.files_1.count).to eq(4)
     end
 
     it "creates a hash of modified files" do
-
+      expect(compare.modified_files.count).to eq("")
     end
 
+    it "creates a hash of moved files" do
+      expect(compare.moved_files.count).to eq("")
+    end
+
+    it "creates a hash of new files" do
+      expect(compare.new_files.count).to eq("")
+    end
   end  
 
 end
