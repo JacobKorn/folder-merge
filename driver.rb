0e6a37620b6c858d24980f50118db9e1ff98c100
Anthony Mace <anthony@anthonymace.com>
2015-09-27 18:26:59 +1300

    removed driver from repo?

--- driver.rb
+++
@@ -1,9 +0,0 @@
-require 'digest/sha1'
-require_relative 'models/folder_entries'
-require_relative 'models/folder_tree'
-require 'pry-byebug'
-
-root_path = "/media/anthony/Seagate Backup Plus Drive/pmc-sync-folders-working-folders/Management/Accounting/Angela Accounting/"
-
-f = FolderTree.new(root_path)
-p f.analyse_tree
