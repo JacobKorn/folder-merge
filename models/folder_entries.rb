1bac4bfe48927341b27fe699541aeae3476a9fbb
Anthony Mace <anthony@anthonymace.com>
2015-10-01 02:27:25 +1300

    switch entries to use recursive Dir.glob

--- models/folder_entries.rb
+++ models/folder_entries.rb
@@ -1,75 +1,37 @@
 class FolderEntries
 
-  def self.entries_hash(root_path)
+  def self.folders_and_files(root_path)
     folder_entries = new(root_path)
-    folder_entries.entries_hash
+    [folder_entries.folder_paths, folder_entries.file_paths]
   end
 
-  def self.folder_and_files_hahses(root_path)
-    folder_entries = new(root_path)
-    [folder_entries.folders_hash, folder_entries.files_hash]
-  end
+  attr_reader :folder_paths, :file_paths
 
   def initialize(root_path)
-    @entries = {}
     @root_path = root_path
-  end
-
-  def folders_hash
-    entries_hash.select do |path, meta|
-      meta[:folder]
-    end
-  end
-
-  def files_hash
-    entries_hash.select do |path, meta|
-      meta[:folder] == false
-    end
-  end
-
-  def entries_hash
-    @entries_hash ||= populate_entries_hash
+    @folder_paths = []
+    @file_paths = []
+    populate_paths
   end
 
 private
 
-  attr_accessor :entries
   attr_reader :root_path
+  attr_writer :folder_paths, :file_paths
+
 
   def entry_paths
-    @entry_paths ||= populate_entry_paths
+    @entry_paths ||= Dir.glob(root_path + "/**/*")
   end
 
-  def populate_entry_paths
-    file_names = Dir.entries(root_path)
-    file_names.delete(".")
-    file_names.delete("..")
-    file_names.map { |entry| "#{root_path}/#{entry}"}
-  end
-
-  def populate_entries_hash
-    entry_paths.each do |file|
-      if File.directory?(file)
-        entries[file] = folder_hash(file)
+  def populate_paths
+    entry_paths.each do |entry_path|
+      if File.directory?(entry_path)
+        self.folder_paths << entry_path
       else
-        entries[file] = file_hash(file)
+        self.file_paths << entry_path
       end
     end
-    entries
-  end
-
-  def folder_hash(folder)
-    {
-      folder: true,
-      scanned: false
-    }
-  end
-
-  def file_hash(file)
-    {
-      folder: false,
-      sha1: Digest::SHA1.hexdigest(IO.read(file))
-    }
   end
 
 end
--- spec/models/folder_entries_spec.rb
+++ spec/models/folder_entries_spec.rb
@@ -5,32 +5,23 @@ RSpec.describe FolderEntries, type: :model do
 
   let(:path) { "./spec/support/diverged_folders/files" }
 
-  it "#entry_paths returns an Array of folder paths" do
+  it "#entry_paths returns an Array of entry paths" do
     folder_entries = FolderEntries.new(path)
     result = folder_entries.send(:entry_paths)
     expect(result).to be_an(Array)
+    expect(result.count).to eq(6)
   end
 
-  it "#entries_hash returns a hash of entries" do
+  it "#folder_paths returns an Array of folder paths" do
     folder_entries = FolderEntries.new(path)
-    expected_result = {"./spec/support/diverged_folders/files/empty_file.txt"=>{:folder=>false, :sha1=>"da39a3ee5e6b4b0d3255bfef95601890afd80709"}, "./spec/support/diverged_folders/files/subfolder"=>{:folder=>true, :scanned=>false}, "./spec/support/diverged_folders/files/quote.txt"=>{:folder=>false, :sha1=>"c506e2c74c23db4db01e2b4133483ce098408f42"}, "./spec/support/diverged_folders/files/essay.txt"=>{:folder=>false, :sha1=>"15b892a2bd36681203e3c2ec61ca2ce5ddc7a418"}, "./spec/support/diverged_folders/files/report.txt"=>{:folder=>false, :sha1=>"29d6162f2e3927a7cfe95c0ee87adbbbae00da35"}}
-    expect(folder_entries.entries_hash).to eq(expected_result)
+    expected_result = ["./spec/support/diverged_folders/files/subfolder"]
+    expect(folder_entries.folder_paths).to eq(expected_result)
   end
 
-  it "#folders_hash only contains folders" do
+  it "#file_paths returns an Array of file paths" do
     folder_entries = FolderEntries.new(path)
-    result = folder_entries.folders_hash.any? do |path, meta|
-      meta[:folder] == false
-    end
-    expect(result).to eq(false)
-  end
-
-  it "#files_hash only contains files" do
-    folder_entries = FolderEntries.new(path)
-    result = folder_entries.files_hash.any? do |path, meta|
-      meta[:folder] == true
-    end
-    expect(result).to eq(true)
+    expected_result = ["./spec/support/diverged_folders/files/empty_file.txt", "./spec/support/diverged_folders/files/subfolder/quote.txt", "./spec/support/diverged_folders/files/quote.txt", "./spec/support/diverged_folders/files/essay.txt", "./spec/support/diverged_folders/files/report.txt"]
+    expect(folder_entries.file_paths).to eq(expected_result)
   end
 
 end
